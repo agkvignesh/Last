366702061299298af8f2abd16f7ce2b052f0b630
agkvignesh <agkvignesh42@gmail.com>
2022-11-07 20:41:39 +0530

    love

---
+++ src/org/array/ForLoop.java
@@ -0,0 +1,40 @@
+package org.array;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+	
+	int a[] = new int [6];
+	a[0] =10;
+	a[1] =20;
+	a[2] =30;
+	a[3] =40;
+	a[4] =50;
+	a[5] =60;
+
+	System.out.println(a[5]);
+	
+	int length = a.length;
+    System.out.println(length);
+    
+	for (int i = 0; i < 6; i++) {
+		System.out.println(a[i]);
+		
+	}
+	                     
+	for (int i : a) {
+		System.out.println(i);
+	}                     
+	                    
+	                     
+	                     
+	                     
+	                     
+	                     
+	                     
+	                     
+	                     
+	                     
+	     
+}
+}
---
+++ src/org/array/SumArray.java
@@ -0,0 +1,16 @@
+package org.array;
+
+public class SumArray {
+	public static void main(String[] args) {
+		int sum=0;
+		 int array[][] = {{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}};
+		 for (int i=1;i<=array.length;i++) {
+			 sum=sum+i;
+			 //System.out.println(sum);
+		 }
+		System.out.println(sum);
+				
+		
+	}
+
+}
